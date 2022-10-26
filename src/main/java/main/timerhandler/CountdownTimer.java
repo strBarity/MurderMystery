d997173b7a4a81a316cc2dcd96a21a31352eae18
Barity_ <mir3292@gmail.com>
2022-10-26 19:01:17 +0900

    플레이어 기준 변경

--- src/main/java/main/timerhandler/CountdownTimer.java
+++ src/main/java/main/timerhandler/CountdownTimer.java
@@ -6,7 +6,7 @@ public class CountdownTimer implements Runnable {
     private static Long startCountdown = 60L;
     @Override
     public void run() {
-        if (Bukkit.getOnlinePlayers().size() > 1) startCountdown--;
+        if (Bukkit.getOnlinePlayers().size() > 2) startCountdown--;
         else startCountdown = 60L;
     } public static Long getStartCountdown() {
         return startCountdown;
