90f1c5d1c8f64e113f10213cdcf2e1c2cf4494cf
Barity_ <mir3292@gmail.com>
2022-10-26 19:00:34 +0900

    커맨드 쿨타임 추가

--- src/main/java/main/cmdhandler/CMDHandler.java
+++ src/main/java/main/cmdhandler/CMDHandler.java
@@ -1,24 +1,38 @@
 package main.cmdhandler;
 
 import main.Main;
-import org.bukkit.Bukkit;
+import main.timerhandler.CMDCooldownTimer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class CMDHandler implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         try {
-            if (s.equals("murder")) MurderHandler.onCommand(commandSender, strings);
+            if (CMDCooldownTimer.getCMDClickStack().containsKey((Player) commandSender)) {
+                for (Map.Entry<Player, Integer> e : CMDCooldownTimer.getCMDClickStack().entrySet()) {
+                    if (e.getKey().equals(commandSender) && e.getValue() >= 4) {
+                        if (e.getValue() < 5) e.setValue(e.getValue()+1);
+                        commandSender.sendMessage(Main.INDEX + "§c커맨드 사용이 너무 빠릅니다. 잠시 후에 다시 시도해주세요.");
+                        return true;
+                    } else {
+                        e.setValue(e.getValue()+1);
+                        break;
+                    }
+                }
+            } else CMDCooldownTimer.getCMDClickStack().put((Player) commandSender, 1);
+            if (s.equals("murder")) MurderTHandler.onCommand(commandSender, strings);
         } catch (Exception e) {
-            Bukkit.getServer().broadcastMessage(Main.INDEX + "§6onCommand§c에서 오류가 발생했습니다: §4" + e.getClass().getSimpleName());
+            Main.s.broadcastMessage(Main.INDEX + "§6onCommand§c에서 오류가 발생했습니다: §4" + e.getClass().getSimpleName());
             e.printStackTrace();
         } return false;
     }
@@ -34,7 +48,7 @@ public class CMDHandler implements TabExecutor {
                 }
             }
         } catch (Exception e) {
-            Bukkit.getServer().broadcastMessage(Main.INDEX + "§6onTabComplete§c에서 오류가 발생했습니다: §4" + e.getClass().getSimpleName());
+            Main.s.broadcastMessage(Main.INDEX + "§6onTabComplete§c에서 오류가 발생했습니다: §4" + e.getClass().getSimpleName());
             e.printStackTrace();
         }
         return null;
