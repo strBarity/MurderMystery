e4b3f17f875265c7c1a4f7975dc892771d2f5241
Barity_ <mir3292@gmail.com>
2022-10-26 19:01:36 +0900

    게임 시스템 클래스와 커맨드 클래스 분리

--- src/main/java/main/cmdhandler/MurderTHandler.java
+++ src/main/java/main/cmdhandler/MurderTHandler.java
@@ -1,19 +1,24 @@
 package main.cmdhandler;
 
 import main.Main;
+import main.gamehandler.MurderHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class MurderHandler {
-    public static boolean gameStarted = false;
+import static main.gamehandler.MurderHandler.gameStarted;
+
+public class MurderTHandler {
     public static void onCommand(CommandSender commandSender, String @NotNull [] args) {
         Player p = (Player) commandSender;
         final String w = Main.INDEX + "§c올바르지 않은 사용법입니다!";
         final String notOp = Main.INDEX + "§c이 명령어를 사용할 권한이 없습니다!";
         if (args.length < 2) {
             p.sendMessage(w);
+            return;
+        } else if (!p.isOp()) {
+            p.sendMessage(notOp);
             return;
         } switch (args[0]) {
             case "task":
@@ -29,11 +34,13 @@ public class MurderHandler {
                     if (!gameStarted) {
                         gameStarted = true;
                         Bukkit.getServer().broadcastMessage(Main.INDEX + "§b관리자가 게임을 시작시켰습니다.");
+                        MurderHandler.startGame();
                     } else p.sendMessage(Main.INDEX + "게임이 이미 진행 중입니다.");
                 } else if (args[1].equals("stop")) {
                     if (gameStarted) {
                         gameStarted = false;
                         Bukkit.getServer().broadcastMessage(Main.INDEX + "§c관리자가 게임을 중지시켰습니다.");
+                        MurderHandler.stopGame();
                     } else p.sendMessage(Main.INDEX + "게임이 진행 중이 아닙니다.");
                 } break;
             default: p.sendMessage(w);
---
+++ src/main/java/main/gamehandler/MurderHandler.java
@@ -0,0 +1,10 @@
+package main.gamehandler;
+
+public class MurderHandler {
+    public static boolean gameStarted = false;
+    public static void startGame() {
+
+    } public static void stopGame() {
+
+    }
+}
