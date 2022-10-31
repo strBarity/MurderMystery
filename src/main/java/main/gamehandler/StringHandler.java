ebd6c8d7b000da2d95c04af67cedb9d2329272e8
ChocoSwi <channel.iru0000@gmail.com>
2022-10-31 22:06:45 +0900

    문자열 색상, 스타일 적용 함수 추가

--- src/main/java/main/gamehandler/StringHandler.java
+++ src/main/java/main/gamehandler/StringHandler.java
@@ -4,7 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StringHandler {
-    public enum MurderLanguage { Korean, English }
+    public enum MurderLanguage {Korean, English}
+
+    public enum StringColor {
+        Black(0), Dark_Blue(1), Dark_Green(2), Dark_Aqua(3), Dark_Red(4), Dark_Purple(5),
+        Gold(6), Gray(7), Dark_Gray(8), Blue(9), Green(0xa), Aqua(0xb), Red(0xc), Light_Purple(0xd),
+        Yellow(0xe), White(0xf);
+
+        public final int value;
+
+        StringColor(int value) {
+            this.value = value;
+        }
+    }
+
+    public enum StringStyle {
+        Obfuscated('k'), Bold('l'), Strikethrough('m'), Underlined('n'), Italic('o'), None('r');
+
+        public final char value;
+
+        StringStyle(char value) {
+            this.value = value;
+        }
+    }
 
     public static Map<String, String> KoreanMessage = new HashMap<>();
     public static Map<String, String> EnglishMessage = new HashMap<>();
@@ -25,5 +47,27 @@ public class StringHandler {
             default:
                 return EnglishMessage.get(key);
         }
+    }
+
+    public static String applyColor(String str, StringColor color) {
+        return applyColor(str, color, color);
+    }
+
+    public static String applyColor(String str, StringColor color, StringColor reset) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("§").append(Integer.toHexString(color.value)).append(str).append("§").append(Integer.toHexString(reset.value));
+        return sb.toString();
+    }
+
+    public static String applyStyle(String str, StringStyle style)
+    {
+        return applyStyle(str, style, false);
+    }
+
+    public static String applyStyle(String str, StringStyle style, boolean reset) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("§").append(style.value).append(str);
+        if (reset) sb.append("§").append(StringStyle.None.value);
+        return sb.toString();
     }
 }
