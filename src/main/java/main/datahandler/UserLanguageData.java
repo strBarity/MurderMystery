a406bdf4d20f0477cc4813376192f0554b36284e
Barity_ <mir3292@gmail.com>
2022-11-01 19:27:39 +0900

    import 문 최적화

--- src/main/java/main/datahandler/UserLanguageData.java
+++ src/main/java/main/datahandler/UserLanguageData.java
@@ -7,8 +7,6 @@ import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserLanguageData {
     private static FileConfiguration userLanguageData;
