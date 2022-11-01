8c9f92088db61897fb0ea0b84d237f50c7499ed3
Barity_ <mir3292@gmail.com>
2022-11-01 19:20:30 +0900

    npc 소환 커맨드 제거

--- src/main/java/main/cmdhandler/MurderTHandler.java
+++ src/main/java/main/cmdhandler/MurderTHandler.java
@@ -1,7 +1,5 @@
 package main.cmdhandler;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
 import main.Main;
 import main.datahandler.SpawnLocationData;
 import main.gamehandler.MurderHandler;
@@ -10,25 +8,15 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.minecraft.server.v1_12_R1.*;
-import org.bukkit.Material;
-import org.bukkit.SoundCategory;
 import org.bukkit.*;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_12_R1.scoreboard.CraftScoreboard;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.UUID;
 
-import static main.Main.sc;
 import static main.gamehandler.MurderHandler.gameStarted;
 
 public class MurderTHandler {
@@ -132,64 +120,6 @@ public class MurderTHandler {
                             }
                         } break;
                 } break;
-            case "npc":
-                if (Bukkit.getPlayer(args[1]) == null) {
-                    p.sendMessage(w);
-                    break;
-                } p.sendMessage(Main.INDEX + "§eNPC 생성 중...");
-                int i1 = 0; int i2 = 0;
-                final float yaw = p.getLocation().getYaw();
-                if (yaw >= -45 && yaw < 45) {
-                    i1 = 104; i2 = 175;
-                } else if (yaw >= 45 && yaw < 135) {
-                    i1 = 105; i2 = 176;
-                } else if ((yaw >= 135 && yaw <= 180) || (yaw >= -180 && yaw < -135)) {
-                    i1 = 104; i2 = 177;
-                } else if (yaw >= -135 && yaw < -45) {
-                    i1 = 103; i2 = 176;
-                } if (i1 == 0) {
-                    double r = Math.random();
-                    if (r <= 0.25) {
-                        i1 = 104; i2 = 175;
-                    } else if (r <= 0.5) {
-                        i1 = 105; i2 = 176;
-                    } else if (r <= 0.75) {
-                        i1 = 104; i2 = 177;
-                    } else {
-                        i1 = 103; i2 = 176;
-                    }
-                } final int x = i1; final int z = i2;
-                Location l = p.getLocation();
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    CraftPlayer c = (CraftPlayer) Bukkit.getPlayerExact(args[1]);
-                    Property property = c.getProfile().getProperties().get("textures").iterator().next();
-                    MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
-                    WorldServer world = ((CraftWorld) Bukkit.getWorlds().get(0)).getHandle();
-                    EntityPlayer npc = new EntityPlayer(server, world, new GameProfile(UUID.randomUUID(), ""), new PlayerInteractManager(world));
-                    ScoreboardTeam team = new ScoreboardTeam(((CraftScoreboard) Bukkit.getScoreboardManager().getMainScoreboard()).getHandle(), p.getName());
-                    team.setNameTagVisibility(ScoreboardTeamBase.EnumNameTagVisibility.NEVER);
-                    team.setCollisionRule(ScoreboardTeamBase.EnumTeamPush.NEVER);
-                    npc.getProfile().getProperties().removeAll("textures");
-                    npc.getProfile().getProperties().put("textures", new Property("textures", property.getValue(), property.getSignature()));
-                    npc.getDataWatcher().set(new DataWatcherObject<>(13, DataWatcherRegistry.a), (byte) 0xFF);
-                    npc.setLocation(l.getX(), l.getY(), l.getZ(), 0F, 0F);
-                    PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
-                    connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, npc));
-                    connection.sendPacket(new PacketPlayOutNamedEntitySpawn(npc));
-                    sc.scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> {
-                        connection.sendPacket(new PacketPlayOutEntityMetadata(npc.getId(), npc.getDataWatcher(), true));
-                        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, npc));
-                        connection.sendPacket(new PacketPlayOutEntityHeadRotation(npc, (byte) 64));
-                        connection.sendPacket(new PacketPlayOutBlockChange(npc.world, new BlockPosition(npc.locX, npc.locY, npc.locZ)));
-                        connection.sendPacket(new PacketPlayOutBed(npc, new BlockPosition(x, 79, z)));
-                        connection.sendPacket(new PacketPlayOutScoreboardTeam(team, 1));
-                        connection.sendPacket(new PacketPlayOutScoreboardTeam(team, 0));
-                        connection.sendPacket(new PacketPlayOutScoreboardTeam(team, Collections.singletonList(npc.getName()), 3));
-                        npc.teleportTo(new Location(p.getWorld(), l.getX(), l.getY(), l.getZ(), 90F, 0F), false);
-                        connection.sendPacket(new PacketPlayOutEntityTeleport(npc));
-                    }, 1L);
-                } sc.scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> p.sendMessage(Main.INDEX + "§bNPC 생성 완료."), 1L);
-                break;
             case "task":
                 if (args[1].equals("cancel")) {
                     Bukkit.getServer().getScheduler().cancelTasks(Main.getPlugin(Main.class));
