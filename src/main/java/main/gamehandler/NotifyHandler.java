761e8d9505057e05d63a91c9ce33ad671df86216
ChocoSwi <channel.iru0000@gmail.com>
2022-11-01 19:30:50 +0900

    NotifyHandler

--- src/main/java/main/gamehandler/NotifyHandler.java
+++ src/main/java/main/gamehandler/NotifyHandler.java
@@ -1,4 +1,17 @@
 package main.gamehandler;
 
+import main.stringhandler.TextFormatter;
+import main.stringhandler.TranslateHandler;
+import org.bukkit.entity.Player;
+
 public class NotifyHandler {
+    public static void sendTitle(Player p, String key, String key1, int i, int i1, int i2)
+    {
+        p.sendTitle(TranslateHandler.getString(key, p), TranslateHandler.getString(key1, p), i, i1, i2);
+    }
+
+    public static void sendTitle(Player p, TextFormatter key, TextFormatter key1, int i, int i1, int i2)
+    {
+
+    }
 }
