471a031b722b4199650b1ab882e5f30af275ddde
Barity_ <mir3292@gmail.com>
2022-11-15 20:47:31 +0900

    추가 상수화

--- src/main/java/main/timerhandler/CountdownTimer.java
+++ src/main/java/main/timerhandler/CountdownTimer.java
@@ -2,13 +2,13 @@ package main.timerhandler;
 
 import main.Main;
 import main.gamehandler.MurderHandler;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 
 import static main.Main.SERVER;
 import static main.Main.printException;
 import static main.eventhandler.EventListener.startPlayerCount;
+import static org.bukkit.Sound.BLOCK_NOTE_HAT;
+import static org.bukkit.SoundCategory.MASTER;
 
 public class CountdownTimer implements Runnable {
     private static Long startCountdown = 60L;
@@ -34,43 +34,43 @@ public class CountdownTimer implements Runnable {
             if (SERVER.getOnlinePlayers().size() >= startPlayerCount && !MurderHandler.gameStarted) {
                 if (startCountdown == 60L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b60§e초 후에 시작합니다!");
                 }
                 startCountdown--;
                 if (startCountdown == 30L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b30§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 10L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b10§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 5L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b5§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 4L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b4§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 3L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b3§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 2L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b2§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 1L) {
                     for (Player p : SERVER.getOnlinePlayers())
-                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HAT, SoundCategory.MASTER, 100F, 1F);
+                        p.playSound(p.getLocation(), BLOCK_NOTE_HAT, MASTER, 100F, 1F);
                     SERVER.broadcastMessage(Main.INDEX + "§e게임이 §b1§e초 후에 시작합니다!");
                 }
                 if (startCountdown == 0L) MurderHandler.startGame(Main.CURRENTMAP);
